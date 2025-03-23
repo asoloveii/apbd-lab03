f75e8ad6d7ff65221241eb9253f5202fd382abce
Arseni Salavei <arsenisoloveii@gmail.com>
2025-03-23 14:37:18 +0100

    feat: liquid type container

---
+++ lab03/Containers/LiquidContainer.cs
@@ -0,0 +1,32 @@
+﻿namespace lab03.Containers;
+
+public class LiquidContainer : Container, IHazardNotifier
+{
+    private bool IsHazardous { get; }
+
+    public LiquidContainer(int maxPayload, bool isHazardous) : base('L', maxPayload)
+    {
+        IsHazardous = isHazardous;
+    }
+
+    public void NotifyHazard()
+    {
+        Console.WriteLine($"[Hazard Alert] Liquid Container {SerialNumber}");
+    }
+
+    public override void EmptyCargo()
+    {
+        CargoWeight = 0;
+    }
+
+    public override void LoadCargo(int payload)
+    {
+        if (IsHazardous)
+        {
+            if (payload + CargoWeight > MaxPayload * 0.5)
+                Console.WriteLine("[Alert] Attempting to load dangerous amount");
+        } else if (payload + CargoWeight > MaxPayload * 0.9) 
+            Console.WriteLine("[Alert] Attempting to load dangerous amount");
+        else CargoWeight += payload;
+    }
+}
