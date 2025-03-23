7a9c5da77b35cb31ebe0a3c49218957a9edfce48
Arseni Salavei <arsenisoloveii@gmail.com>
2025-03-23 14:51:20 +0100

    feat: gas type container

---
+++ lab03/Containers/GasContainer.cs
@@ -0,0 +1,31 @@
+﻿using lab03.Exceptions;
+
+namespace lab03.Containers;
+
+public class GasContainer : Container, IHazardNotifier
+{
+    private bool IsHazardous { get; }
+    private int Pressure { get; set; } // in atmospheres
+    
+    public GasContainer(int maxPayload, int pressure, bool isHazardous) : base('G', maxPayload)
+    {
+        IsHazardous = isHazardous;
+        Pressure = pressure;
+    }
+
+    public void NotifyHazard()
+    {
+        Console.WriteLine($"[Hazard Alert] Gas Container {SerialNumber}");
+    }
+
+    public override void EmptyCargo()
+    {
+        CargoWeight *= (int) 0.05;
+    }
+
+    public override void LoadCargo(int payload)
+    {
+        if (CargoWeight + payload > MaxPayload) throw new OverfillException("[Exception] Gas Container overflow");
+        CargoWeight += payload;
+    }
+}
